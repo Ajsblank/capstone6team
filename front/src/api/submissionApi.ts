98c2571249647909870f91242ede659bd65a617c
seopark428 <seopark17@gmail.com>
2026-03-31 18:15:59 +0900

    chore: 서버 주소 env 파일 저장

--- front/src/api/submissionApi.ts
+++ front/src/api/submissionApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { SubmitRequest, SubmitResponse } from "../types";
 
-export const SUBMIT_URL = "http://98.92.116.47:8080/api/code/submit";
+export const SUBMIT_URL = `${process.env.REACT_APP_API_BASE_URL}/api/code/submit`;
 
 export const submitCode = async (payload: SubmitRequest): Promise<SubmitResponse> => {
   const response = await axios.post<SubmitResponse>(SUBMIT_URL, payload);
