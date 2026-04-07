0298cfe595b2e53e8626b7efc30a3a017364746d
Lee37373 <asdfqwer1234@ajou.ac.kr>
2026-04-07 21:56:22 +0900

    fix: @Valid 어노테이션 추가, 인코딩 설정

--- server/src/main/java/com/asap/server/api/controller/CodeController.java
+++ server/src/main/java/com/asap/server/api/controller/CodeController.java
@@ -2,6 +2,7 @@ package com.asap.server.api.controller;
 
 import com.asap.server.api.dto.request.CodeSubmitRequest;
 import com.asap.server.api.dto.response.CodeSubmitResponse;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.*;
 public class CodeController {
     
     @PostMapping("/submit")
-    public ResponseEntity<CodeSubmitResponse> submitCode(@RequestBody CodeSubmitRequest request) {
+    public ResponseEntity<CodeSubmitResponse> submitCode(@Valid @RequestBody CodeSubmitRequest request) {
+        
+        log.info("\nuserId : "+request.getUserId()+"\nlanguage : "+request.getLanguage()+"\n"+request.getSourceCode());
         
         CodeSubmitResponse responseData = new CodeSubmitResponse(true, "코드가 서버에 성공적으로 제출되었습니다!");
         
