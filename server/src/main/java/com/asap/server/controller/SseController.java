0a1f3ceab1c72eaa572a62569b9cd5a88e42598c
LeeJaeHyeok <asdfqwer1234@ajou.ac.kr>
2026-04-28 20:57:04 +0900

    feat: 채점 후 결과 DB 저장 및 Sse방식으로 Client에 전달 기능 구현

---
+++ server/src/main/java/com/asap/server/controller/SseController.java
@@ -0,0 +1,23 @@
+package com.asap.server.controller;
+
+import com.asap.server.service.SseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequestMapping("/api/subscribe")
+@RequiredArgsConstructor
+public class SseController {
+
+    private final SseService sseService;
+
+    @GetMapping(value = "/{userId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@PathVariable Long userId) {
+        return sseService.subscribe(userId);
+    }
+}
---
+++ server/src/main/java/com/asap/server/dto/response/CodeBattleMatchResult.java
@@ -0,0 +1,14 @@
+package com.asap.server.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeBattleMatchResult {
+    private Long matchId;
+    private String winner;
+    private String log;
+}
