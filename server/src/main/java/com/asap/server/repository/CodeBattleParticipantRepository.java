8e35261792edeca08fab49f5b36af872e76ad07f
Ajsblank <ajs8780@ajou.ac.kr>
2026-05-06 16:25:20 +0900

    chore: 임시 저장

--- server/src/main/java/com/asap/server/repository/CodeBattleParticipantRepository.java
+++ server/src/main/java/com/asap/server/repository/CodeBattleParticipantRepository.java
@@ -1,11 +1,11 @@
 package com.asap.server.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 import com.asap.server.domain.CodeBattleParticipant;
 
