444ed735ee92c4bc3dbccf79629f94bc434351ab
LeeJaeHyeok <asdfqwer1234@ajou.ac.kr>
2026-05-17 16:30:55 +0900

    fix: 오류 수정

--- server/src/main/java/com/asap/server/global/RedisResultWorker.java
+++ server/src/main/java/com/asap/server/global/RedisResultWorker.java
@@ -177,7 +177,7 @@ public class RedisResultWorker implements CommandLineRunner {
             JsonNode rootNode = objectMapper.readTree(rawData);
             
             // 채점 서버가 보내주는 데이터 타입에 맞게 파싱 방법을 선택하세요 (String vs Long)
-            String userIdStr = rootNode.get("userid").asText();
+            String userIdStr = rootNode.get("userId").asText();
             Long targetUserId = Long.parseLong(userIdStr); 
             
             String resultLog = rootNode.get("log").asText();
