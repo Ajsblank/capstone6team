f9ee0df2861a2ac79c392f58518249da98736513
Ajsblank <ajs8780@ajou.ac.kr>
2026-05-28 23:59:38 +0900

    feat: 대회 개최 시 샘플 코드를 지원 언어와 리스트로 받도록 수정

--- server/src/main/java/com/asap/server/repository/CodeBattleSampleCodeRepository.java
+++ server/src/main/java/com/asap/server/repository/CodeBattleSampleCodeRepository.java
@@ -1,8 +1,11 @@
 package com.asap.server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.asap.server.domain.CodeBattleSampleCode;
 
 public interface CodeBattleSampleCodeRepository extends JpaRepository<CodeBattleSampleCode, Long> {
+  List<CodeBattleSampleCode> findByContestIdOrderBySampleOrderAsc(Long contestId);
 }
